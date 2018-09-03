031ed0c24de90ebf812312121fa939c68202dd3e
huynhit92 <pham.huynh@donuts.ne.jp>
2018-09-03 23:43:51 +0900

    complete #4

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Shaun'
+    name: 'Shaun',
+    job: 'ninja',
+    website: 'http://www.theninja.com',
+    tag: '<a href="http://www.theninja.com">tag website</a>'
+  },
+  methods: {
+    greet: function(time){
+      return 'Good ' + time + this.name;
+    }
   }
 });
